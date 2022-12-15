ed0ccd668c285e71f783ed2c1a5ca533c576576b
Tiin24 <agustiinromero2401@gmail.com>
2022-12-15 15:03:03 -0300

    se agrego el scroll

--- src/components/Home/Contenido/Contenido.jsx
+++ src/components/Home/Contenido/Contenido.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React,{useEffect} from "react";
 import style from "./Contenido.module.css";
-
+import Aos from "aos";
+import 'aos/dist/aos.css'
 function Contenido() {
+  useEffect(() => {
+    Aos.init({duration:2000})
+  })
   return (
     <>
       <section>
         <div className={style.elementor_container}>
           <div className={style.container_left}>
             <div className={style.title_container}>
-              <h2 className={style.titulo}>NUESTRAS DONUTS</h2>
+              <h2 data-aos='fade-right' className={style.titulo}>NUESTRAS DONUTS</h2>
             </div>
             <div className={style.parrafo_contain}>
-              <p className={style.Parrafo}>
+              <p data-aos='fade-right' className={style.Parrafo}>
                 Alegrando la Vida. Horneamos nuestra filosofía, que cada bocado
                 de nuestra Donuts sea tan bueno como el momento que lo rodea.
                 Hemos creado miles de variedades de Donuts y estamos orgullosos
@@ -19,13 +23,14 @@ function Contenido() {
               </p>
             </div>
             <div className={style.buttom_contain}>
-              <button className={style.Boton}>
+              <button data-aos='fade-right' className={style.Boton}>
                 <p className={style.boton_p}>VER MÁS</p>
               </button>
             </div>
           </div>
           <div className={style.container_rigth}>
             <img
+              data-aos='fade-left'
               src="https://dunkin.cl/wp-content/uploads/2022/04/donuts-inicio-chile-300x300.png"
               width="300"
               height="300"
@@ -38,6 +43,7 @@ function Contenido() {
         <div className={style.elementor_container}>
           <div className={style.container_rigth_2}>
             <img
+              data-aos='fade-left'
               src="https://dunkin.cl/wp-content/uploads/2022/04/Cafes-Bebidas-dunkin-600-267x300.png"
               width="267"
               height="300"
@@ -46,12 +52,12 @@ function Contenido() {
           </div>
           <div className={style.container_left}>
             <div className={style.title_container}>
-              <h2 className={style.titulo}>
+              <h2 data-aos='fade-right' className={style.titulo}>
                 EL MEJOR CAFÉ COMIENZA CON EL MEJOR GRANO.
               </h2>
             </div>
             <div className={style.parrafo_contain}>
-              <p className={style.Parrafo}>
+              <p data-aos='fade-right' className={style.Parrafo}>
                 Compartimos energía Positiva en cada taza de Café. Cultivamos
                 relaciones duraderas con los cafeteros para obtener los mejores
                 granos de las zonas cafeteras, 100% arábicos y con certificación
@@ -59,7 +65,7 @@ function Contenido() {
               </p>
             </div>
             <div className={style.buttom_contain}>
-              <button className={style.Boton}>
+              <button data-aos='fade-right' className={style.Boton}>
                 <p className={style.boton_p}>VER MÁS</p>
               </button>
             </div>
@@ -70,22 +76,23 @@ function Contenido() {
         <div className={style.elementor_container}>
           <div className={style.container_left}>
             <div className={style.title_container}>
-              <h2 className={style.titulo}>SÁNDWICHES, WRAPS Y ENSALADAS</h2>
+              <h2 data-aos='fade-right' className={style.titulo}>SÁNDWICHES, WRAPS Y ENSALADAS</h2>
             </div>
             <div className={style.parrafo_contain}>
-              <p className={style.Parrafo}>
+              <p data-aos='fade-right' className={style.Parrafo}>
                 Sabemos que cada persona tiene sus gustos y en Dunkin’ tenemos
                 el sándwich perfecto para cada momento del día.
               </p>
             </div>
             <div className={style.buttom_contain}>
-              <button className={style.Boton}>
+              <button data-aos='fade-right' className={style.Boton}>
                 <p className={style.boton_p}>VER MÁS</p>
               </button>
             </div>
           </div>
           <div className={style.container_rigth3}>
             <img
+              data-aos='fade-left'
               src="https://dunkin.cl/wp-content/uploads/2022/04/sandwich-dunkin-chile-300x238.png"
               width="300"
               height="238"
--- src/components/Home/Formulario/Formulario.jsx
+++ src/components/Home/Formulario/Formulario.jsx
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import style from "./Formulario.module.css";
 import { BiPaperPlane } from "react-icons/bi";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function Formulario() {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
   return (
     <div className={style.container}>
       <div className={style.container_form}>
         <div className={style.contain_title}>
-          <h2 className={style.Title}>
+          <h2 data-aos="fade-right" className={style.Title}>
             ¡UN DUNKIN' LOVER RECIBE NUESTRAS NOVEDADES!
           </h2>
         </div>
         <div className={style.contain_formulario}>
           <form action="" className={style.formulario}>
-            <div className="contain_content">
+            <div data-aos="fade-left" className="contain_content">
               <div className={style.contain_inputs}>
                 <label className={style.label_F}>Nombre</label>
                 <input type="text" placeholder="Nombre" />
@@ -30,14 +35,14 @@ function Formulario() {
                 <label className={style.label_F}>Fecha de nacimiento</label>
                 <input type="date" placeholder="Fecha Nacimiento" />
               </div>
-              <div className={style.contain_button}>
-                <button className={style.button_S} type="submit">
-                  <BiPaperPlane />
-                  <span className={style.spanF}>ENVIAR</span>
-                </button>
-              </div>
             </div>
           </form>
+          <div className={style.contain_button}>
+            <button data-aos='fade-right' className={style.button_S} type="submit">
+              <BiPaperPlane />
+              <span className={style.spanF}>ENVIAR</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
